5600860f720492c79456fb98de1cc9337f1ed8ce
Andrus Rinde <andrusrinde@users.noreply.github.com>
2018-09-10 10:40:52 +0300

    html muudetud php-ks

---
+++ index_3.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<title>Andrus Rinde, õppetöö</title>
+  </head>
+  <body>
+    <h1>Andrus Rinde</h1>
+	<p>See leht on valminud <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames ja ei oma mingisugust, mõtestatud või muul moel väärtuslikku sisu.</p>
+	<!--<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">-->
+	<img src="../tlu_terra_600x400_1.jpg" alt="TLÜ Terra õppehoone">
+	<p>Mul on sõber, kes ka teeb <a href="../../../~merivor">veebi</a>.</p>
+  </body>
+</html>
