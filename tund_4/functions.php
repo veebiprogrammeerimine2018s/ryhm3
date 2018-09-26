11bc7fae3d88f3ea559f1898fbdc85f3fc734e0e
Andrus Rinde <andrusrinde@users.noreply.github.com>
2018-09-26 14:58:26 +0300

    Neljanda tunni tööd on täiendatud sõnumite lugemisega

--- tund_4/functions.php
+++ tund_4/functions.php
@@ -23,6 +23,21 @@ function saveAMsg($msg){
   $mysqli->close();
   return $notice;
 }
+
+  function readallmessages(){
+	$notice = "";
+	$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	$stmt = $mysqli->prepare("SELECT message FROM vpamsg3");
+	echo $mysqli->error;
+	$stmt->bind_result($msg);
+	$stmt->execute();
+	while($stmt->fetch()){
+		$notice .= "<p>" .$msg ."</p> \n";
+	}
+	$stmt->close();
+	$mysqli->close();
+	return $notice;
+  }
 ?>
 
 
